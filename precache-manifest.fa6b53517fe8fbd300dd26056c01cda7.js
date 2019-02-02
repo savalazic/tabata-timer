c68fc8f6be4b2f1d706ca3c861ec264bfa9a0259
Sava Lazic <savalazic9@gmail.com>
2019-02-02 19:27:57 +0100

    Updates

---
+++ precache-manifest.fa6b53517fe8fbd300dd26056c01cda7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "111a056bd351b4b872c577b3729ac264",
+    "url": "/tabata-timer/static/media/warning.111a056b.mp3"
+  },
+  {
+    "revision": "39da0fb3d25bd4ff4b7aa7df0464faae",
+    "url": "/tabata-timer/static/media/start.39da0fb3.wav"
+  },
+  {
+    "revision": "a3a7b812ca04773a082340d7855ca32d",
+    "url": "/tabata-timer/static/media/end.a3a7b812.wav"
+  },
+  {
+    "revision": "b5c21f3b2804ccba1132e88f14e32e5e",
+    "url": "/tabata-timer/static/media/end-round.b5c21f3b.wav"
+  },
+  {
+    "revision": "24e909732875106b4413",
+    "url": "/tabata-timer/static/js/runtime~main.24e90973.js"
+  },
+  {
+    "revision": "a7500527c70af7d6b676",
+    "url": "/tabata-timer/static/js/main.a7500527.chunk.js"
+  },
+  {
+    "revision": "ef4d08599f9d59715a86",
+    "url": "/tabata-timer/static/js/1.ef4d0859.chunk.js"
+  },
+  {
+    "revision": "28c0656bce7cd65018d3966bde12960c",
+    "url": "/tabata-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tabata-timer/precache-manifest.a5cdf9e53c8b7981caa15cebd888c82b.js"
+  "/tabata-timer/precache-manifest.fa6b53517fe8fbd300dd26056c01cda7.js"
 );
 
 workbox.clientsClaim();
